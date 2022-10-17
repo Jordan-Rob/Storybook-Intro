0a8e4e05eff592b11fefe492e13ab4036ca3239b
Jordan-Rob <jordanrob709@gmail.com>
2022-10-17 10:09:20 +0300

    modified redux store to retrieve data from remote API

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,7 +2,7 @@
 /* A simple redux store/actions/reducer implementation.
  * A true app would be more complex and separated into different files.
  */
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 /*
  * The initial state of our store when the app loads.
@@ -21,10 +21,31 @@ const TaskBoxData = {
 };
 
 /*
+ * Creates an asyncThunk to fetch tasks from a remote endpoint.
+ * You can read more about Redux Toolkit's thunks in the docs:
+ * https://redux-toolkit.js.org/api/createAsyncThunk
+ */
+
+
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/todos?userId=1'
+  );
+  const data = await response.json();
+  const result = data.map((task) => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX',
+  }));
+  return result;
+});
+
+/*
  * The store is created here.
  * You can read more about Redux Toolkit's slices in the docs:
  * https://redux-toolkit.js.org/api/createSlice
  */
+
 const TasksSlice = createSlice({
   name: 'taskbox',
   initialState: TaskBoxData,
@@ -38,6 +59,8 @@ const TasksSlice = createSlice({
     },
   },
 });
+
+
 
 // The actions contained in the slice are exported for usage in our components
 export const { updateTaskState } = TasksSlice.actions;
